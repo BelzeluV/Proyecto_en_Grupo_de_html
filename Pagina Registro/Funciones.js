6eec8d76012e9f0b25cd55654b7b31532d52ab9a
BelzeluV <101567566+BelzeluV@users.noreply.github.com>
2022-05-12 04:37:02 -0400

    V1.7
    agregué un archivo para ver si las validaciones estaban funcionando, funcionan, pero las funciones songelasn la pagina, en la mañana vere como lo hago

--- Pagina Registro/Funciones.js
+++ Pagina Registro/Funciones.js
@@ -5,6 +5,7 @@ function validarcamposvacios()
     let rut = document.getElementById('rut').value;
     let dv = document.getElementById('dv').value;
     let correo = document.getElementById('Correo').value;
+    let correo = document.getElementById('email_contacto').value;
     let contraseña = document.getElementById('contraseña').value;
     let contraseña2 = document.getElementById('contraseñarepite').value;
     let fechanacimiento = document.getElementById('nacimiento').value;
@@ -14,15 +15,99 @@ function validarcamposvacios()
     let comuna = document.getElementById('comuna').value;
 
     if( nombreusuario =="" || real =="" ||rut =="" ||dv=="" ||correo ==""||contraseña == "" || contraseña2 == ""| fechanacimiento== "" || sexo == ""| telefono == "" || direccion == "" || comuna =="")
+    if(nombreusuario==""||real==""||rut==""||dv==""||correo ==""||contraseña==""||contraseña2 ==""||fechanacimiento==""||sexo==""|telefono==""||direccion==""||comuna=="")
     {
         alert("tiene campos sin llenar en el formulario");
         return;
     }
+    
+}
+
+function validarRut()
+{
+    let rut = document.getElementById('rut').value;
+    let tamañorut = rut.length;
+    let dv = document.getElementById('dv').value;
+    var suma = 0;
+    var multiplicador = 3;
+    var i = 1
+    var resto = suma %11;
+    var res_aux = 'k'; 
+    
+    if(rut.length < 7){ 
+        alert("el rut está incompleto");
+        return;
+    }
+
+    if (rut == 7 ){s
+        multiplicador = 2;
+    }
+
+    while (i < tamañorut){
+    
+        suma = suma + rut.substr(i,1) * multiplicador;
+
+        multiplicador = multiplicador -1
+        if (multiplicador = 1){
+            multiplicador = 7;
+        }
+        i = i++;
+    }
+    var resto = suma %11;
+    var resultado = 11-resto;
+
+    if (resultado == dv || res_aux == dv){
+        let mensaje = "Rut Valido.";
+        document.getElementById("lblmensaje").innerHTML = mensaje;
+    }
     else
     {
-    
+        alert("rut invalido, por favor introduzca un rut valido");
     }
 }
-function validarRut(){
 
+
+
+
+
+function validarcontraseña(){
+    let contraseña = document.getElementById('contraseña').value;
+    let contraseña2 = document.getElementById('contraseñarepite').value;
+    var espacio = false;
+    var cont = 0;
+
+    while (!espacio && (cont < contraseña.length)) {
+    if (contraseña.charAt(cont) == " ")
+        espacio = true;
+    cont++;
+    }
+    
+    if (espacio) {
+    alert ("La contraseña no puede contener espacios en blanco");
+    return false;
+    }
+
+    if (contraseña.length == 0 || contraseña2.length == 0) {
+        alert("Los campos de la password no pueden quedar vacios");
+        return;
+    }
+    if (p1 != p2){
+        let mensaje = "las contraseñas coinciden";
+        document.getElementById("lblmensajecontraseña").innerHTML = mensaje;
+        return;
+    }
+    
+}
+
+function validaredad() {
+    let fechanacimiento = document.getElementById('nacimiento').value;
+    var hoy = new Date();
+    var edad = hoy.getFullYear() - fechanacimiento.getFullYear();
+    var m = hoy.getMonth() - fechanacimiento.getMonth();
+    if (m < 0 || (m === 0 && hoy.getDate() < fechanacimiento.getDate())) {
+        edad--;
+    }
+    if(edad < 18){
+        alert("Eres menor de edad, no puedes comprar aqui ningún articulo.");
+    }
 }
